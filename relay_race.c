942ebba6a09ad77bc45163723fc30df66af13fe5
Sergei Puchinin <sergexmax@yandex.ru>
2019-11-25 04:19:25 +0300

    Add relay race

--- relay_race.c
+++ relay_race.c
@@ -1,5 +1,6 @@
 /* See the LICENSE file for copyright and license details. */
 
+#include <sys/msg.h>
 #include <errno.h>
 #include <getopt.h>
 #include <pthread.h>
@@ -23,16 +24,32 @@ enum {
 /* args for the thread function 'foo' */
 typedef struct {
         int thread_id;
+        int msgid;
 } Args;
+
+/* message buffer */
+typedef struct {
+        long mtype;
+} MsgBuf;
 
 /* a thread function that prints a thread id */
 void *
 foo(void *args_void)
 {
         Args *args;
+        MsgBuf msgbuf;
 
         args = (Args *)args_void;
+        if (msgrcv(args->msgid, &msgbuf, 0, args->thread_id, 0)) {
+                perror("msgrcv");
+                exit(errno);
+        }
         printf("Hello World! I'm the %dth thread.\n", args->thread_id);
+        ++msgbuf.mtype;
+        if (msgsnd(args->msgid, &msgbuf, 0, IPC_NOWAIT)) {
+                perror("msgsnd");
+                exit(errno);
+        }
 
         return NULL;
 }
@@ -43,50 +60,74 @@ main(int argc, char *argv[])
         int nthreads; /* Number of threads */
         pthread_t *threads;
         Args *threads_args;
+        int msgid;
+        MsgBuf msgbuf;
 
         { /* Parse options */
-        int opt;
-        bool n_found;
+                int opt;
+                bool n_found;
 
-        n_found = false;
-        while ((opt = getopt(argc, argv, "n:")) != -1) {
-                switch (opt) {
-                case 'n': {
-                        char *end;
-                        nthreads = strtol(optarg, &end, 10);
-                        if (*end != '\0') {
-                                fprintf(stderr, "Wrong format of 'nthreads'.\n");
-                                PRINT_USAGE_HINT();
-                                exit(WRONG_FORMAT);
+                n_found = false;
+                while ((opt = getopt(argc, argv, "n:")) != -1) {
+                        switch (opt) {
+                        case 'n': {
+                                char *end;
+                                nthreads = strtol(optarg, &end, 10);
+                                if (*end != '\0') {
+                                        fprintf(stderr, "Wrong format of 'nthreads'.\n");
+                                        PRINT_USAGE_HINT();
+                                        exit(WRONG_FORMAT);
+                                }
+                                if (nthreads < NTHREADS_MIN || nthreads > NTHREADS_MAX) {
+                                        fprintf(stderr, "The 'nthreads' value is out of range.\n");
+                                        PRINT_USAGE_HINT();
+                                        exit(OUT_OF_RANGE);
+                                }
+                                n_found = true;
+                                break;
                         }
-                        if (nthreads < NTHREADS_MIN || nthreads > NTHREADS_MAX) {
-                                fprintf(stderr, "The 'nthreads' value is out of range.\n");
+                        default:
                                 PRINT_USAGE_HINT();
-                                exit(OUT_OF_RANGE);
+                                exit(WRONG_USAGE);
+                                break;
                         }
-                        n_found = true;
-                        break;
                 }
-                default:
-                        PRINT_USAGE_HINT();
-                        exit(WRONG_USAGE);
-                        break;
+                if (!n_found) {
+                        nthreads = NTHREADS_DEF;
                 }
-        }
-        if (!n_found) {
-                nthreads = NTHREADS_DEF;
-        }
         }
 
-        threads = (pthread_t *)malloc(nthreads * sizeof(*threads));
-        threads_args = (Args *)malloc(nthreads * sizeof(*threads_args));
+        if ((msgid = msgget(IPC_PRIVATE, IPC_CREAT | 0666) < 0)) {
+                perror("msgget");
+                exit(errno);
+        }
+        msgbuf = (MsgBuf){1};
+        if (msgsnd(msgid, &msgbuf, 0, IPC_NOWAIT)) {
+                perror("msgsnd");
+                exit(errno);
+        }
+        if ((threads = (pthread_t *)malloc(nthreads * sizeof(*threads))) == NULL) {
+                perror("malloc");
+                exit(errno);
+        }
+        if ((threads_args = (Args *)malloc(nthreads * sizeof(*threads_args))) == NULL) {
+                perror("malloc");
+                exit(errno);
+        }
         for (int i = 0; i < nthreads; ++i) {
-                threads_args[i] = (Args){i + 1};
+                threads_args[i] = (Args){i + 1, msgid};
                 if (pthread_create(&threads[i], NULL, foo, &threads_args[i])) {
                         perror("pthread_create");
                         exit(errno);
                 }
         }
+
+        if (msgrcv(msgid, &msgbuf, 0, nthreads + 1, 0)) {
+               perror("msgrcv");
+               exit(errno);
+        }
+
+        printf("Hello World!\n");
 
         for (int i = 0; i < nthreads; ++i) {
                 if (pthread_join(threads[i], NULL)) {
@@ -94,7 +135,10 @@ main(int argc, char *argv[])
                         exit(errno);
                 }
         }
-
+        if (msgctl(msgid, IPC_RMID, NULL)) {
+                perror("msgctl");
+                exit(errno);
+        }
         free(threads_args);
         free(threads);
 
